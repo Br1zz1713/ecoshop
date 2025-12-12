6364cb8d738eb07a464b24d2ead0eae6d088205a
Eugene <evgen19092002@gmail.com>
2025-12-12 04:09:39 +0200

    Fix make_superuser command for email auth

--- shop/management/commands/make_superuser.py
+++ shop/management/commands/make_superuser.py
@@ -1,40 +1,41 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 class Command(BaseCommand):
-    help = 'Grant superuser privileges to an existing user'
+    help = 'Make a user a superuser or create one if email is provided'
 
     def add_arguments(self, parser):
-        parser.add_argument('username', type=str, help='Username to grant superuser privileges')
-        parser.add_argument(
-            '--revoke',
-            action='store_true',
-            help='Revoke superuser privileges instead of granting them',
-        )
+        parser.add_argument('identifier', type=str, help='Email or username of the user')
+        parser.add_argument('--revoke', action='store_true', help='Revoke superuser status')
 
     def handle(self, *args, **options):
-        username = options['username']
+        identifier = options['identifier']
         revoke = options.get('revoke', False)
 
+        # Try to find user by email first, then by username
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=identifier)
         except User.DoesNotExist:
-            raise CommandError(f'User "{username}" does not exist')
+            # If not found by email, try username (if field exists)
+            if hasattr(User, 'username'):
+                try:
+                    user = User.objects.get(username=identifier)
+                except User.DoesNotExist:
+                    self.stdout.write(self.style.ERROR(f'User with email or username "{identifier}" does not exist.'))
+                    return
+            else:
+                self.stdout.write(self.style.ERROR(f'User with email "{identifier}" does not exist.'))
+                return
 
         if revoke:
-            user.is_staff = False
             user.is_superuser = False
+            user.is_staff = False
             user.save()
-            self.stdout.write(
-                self.style.SUCCESS(f'Successfully revoked superuser privileges from "{username}"')
-            )
+            self.stdout.write(self.style.SUCCESS(f'Successfully revoked superuser privileges from {identifier}'))
         else:
-            user.is_staff = True
             user.is_superuser = True
+            user.is_staff = True
             user.save()
-            self.stdout.write(
-                self.style.SUCCESS(f'Successfully granted superuser privileges to "{username}"')
-            )
+            self.stdout.write(self.style.SUCCESS(f'Successfully made {identifier} a superuser'))
